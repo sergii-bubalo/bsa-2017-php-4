17261b92b27fb9fddb5625c298df2ce8434c5c00
Sergii Bubalo <sergii.bubalo@gmail.com>
2017-07-09 23:03:37 +0300

    Exception added

---
+++ src/Game/Exceptions/EmptyException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BinaryStudioAcademy\Game\Exceptions;
+
+class EmptyException extends \Exception
+{
+
+}
---
+++ src/Game/Exceptions/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rabzy
+ * Date: 09.07.2017
+ * Time: 23:03
+ */
+
+namespace BinaryStudioAcademy\Game\Exceptions;
+
+
+class NotFoundException extends \Exception
+{
+
+}
